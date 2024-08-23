b28bbce029a085305e255ac81f1cae97bf6eadd6
Marwinez <gietus579@gmail.com>
2024-08-23 13:17:35 +0200

    - Finished testfile.js in part1b - Added part1c with new test file

--- Other_files/part1b/testfile.js
+++ Other_files/part1b/testfile.js
@@ -140,3 +140,71 @@ console.log(rest)          // [3, 4, 5] is printed
 
 // const result = average(2, 5)
 // result is now 3.5
+
+
+
+// ============================
+// OBJECT METHODS AND THIS
+// ============================
+
+// const arto = {
+//   name: 'Arto Hellas',
+//   age: 35,
+//   education: 'PhD',
+
+//   greet: function() {
+//     console.log('hello, my name is ' + this.name)
+//   },
+//   doAddition: function(a, b) {
+//     console.log(a + b)
+//   }
+// }
+
+// arto.greet()  // "hello, my name is Arto Hellas" gets printed
+
+// arto.growOlder = function() {
+//   this.age += 1
+// }
+
+// console.log(arto.age)   // 35 is printed
+// arto.growOlder()
+// console.log(arto.age)   // 36 is printed
+
+// arto.doAddition(1, 4)        // 5 is printed
+
+// const referenceToAddition = arto.doAddition
+// referenceToAddition(10, 15)   // 25 is printed
+
+// const arto = {
+//   name: 'Arto Hellas',
+//   greet: function() {
+//     console.log('hello, my name is ' + this.name)
+//   },
+// }
+
+// setTimeout(arto.greet, 1000)
+
+// setTimeout(arto.greet.bind(arto), 1000)
+
+
+
+
+// ============================
+// CLASSES
+// ============================
+
+class Person {
+  constructor(name, age) {
+    this.name = name
+    this.age = age
+  }
+  greet() {
+    console.log('hello, my name is ' + this.name)
+  }
+}
+
+const adam = new Person('Adam Ondra', 29)
+adam.greet()
+
+const janja = new Person('Janja Garnbret', 23)
+janja.greet()
