682b2a246b00700100f4e08d193ca9a27b4fd196
Marwinez <gietus579@gmail.com>
2024-08-23 16:06:40 +0200

    Modified /Other_files/part1 app (for testing new functionalities)

--- Other_files/part1/src/App.jsx
+++ Other_files/part1/src/App.jsx
@@ -22,18 +22,200 @@ const App = () => {
   )
 }*/
 
+// const App = () => {
+//   const friends = [
+//     { name: 'Peter', age: 4 },
+//     { name: 'Maya', age: 10 }
+//   ]
+
+//   return (
+//     <div>
+//       <p>{friends[0].name} {friends[0].age}</p>
+//       <p>{friends[1].name} {friends[1].age}</p>
+//     </div>
+//   )
+// }
+
+
+// ===================================
+// PART 1C
+// ===================================
+
+// const Hello = ({ name, age }) => {
+//   const bornYear = () => new Date().getFullYear() - age
+
+//   return (
+//       <div>
+//           <p>Hello {name}, you are {age} years old</p>
+//           <p>So you were probably born in {bornYear()}</p>
+//       </div>
+//   )
+// }
+
+// const App = () => {
+//   const name = 'Peter'
+//   const age = 10
+
+//   return (
+//       <div>
+//           <h1>Greetings</h1>
+//           <Hello name="Maya" age={26 + 10} />
+//           <Hello name={name} age={age} />
+//       </div>
+//   )
+// }
+
+
+// const App = (props) => {
+//   const {counter} = props
+//   return (
+//     <div>{counter}</div>
+//   )
+// }
+
+// import { useState } from 'react'
+
+// const Display = ({ counter }) => <div>{counter}</div>
+
+
+// const Button = ({ onClick, text}) =><button onClick={onClick}>{text}</button>
+
+
+
+// const App = () => {
+//   const [ counter, setCounter ] = useState(0)
+//   console.log('rendering with counter value', counter)
+
+//   const increaseByOne = () => {
+//     console.log('increasing, value before', counter)
+//     setCounter(counter + 1)
+//   }
+
+//   const setToZero = () => {
+//     console.log('resetting to zero, value before', counter)
+//     setCounter(0)
+//   }
+
+//   const decreaseByOne = () => {
+//     console.log('decreasing, value before', counter)
+//     setCounter(counter - 1)
+//   }
+
+//   return (
+//     <div>
+//       <Display counter={counter} />
+//       <Button onClick={increaseByOne} text='plus' />
+//       <Button onClick={setToZero} text='zero' />
+//       <Button onClick={decreaseByOne} text='minus' />
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+// ===================================
+// PART 1D
+// ===================================
+
+// import { useState } from 'react'
+
+// const History = (props) => {
+//   if (props.allClicks.length === 0) {
+//     return (
+//       <div>
+//         the app is used by pressing the buttons
+//       </div>
+//     )
+//   }
+//   return (
+//     <div>
+//       button press history: {props.allClicks.join(' ')}
+//     </div>
+//   )
+// }
+
+// const Button = ({ handleClick, text}) =><button onClick={handleClick}>{text}</button>
+
+
+// const App = () => {
+// /*  const [clicks, setClicks] = useState({
+//     left: 0, right: 0
+//   })
+
+//   const handleLeftClick = () =>
+//     setClicks({ ...clicks, left: clicks.left + 1 })
+  
+//   const handleRightClick = () =>
+//     setClicks({ ...clicks, right: clicks.right + 1 })*/
+
+//   const [left, setLeft] = useState(0)
+//   const [right, setRight] = useState(0)
+//   const [allClicks, setAll] = useState([])
+//   const [total, setTotal] = useState(0)
+
+//   const handleLeftClick = () => {
+//     setAll(allClicks.concat('L'))
+//     const updatedLeft = left + 1
+//     setLeft(updatedLeft)
+//     setTotal(updatedLeft + right)
+//   }
+
+//   const handleRightClick = () => {
+//     setAll(allClicks.concat('R'))
+//     const updatedRight = right + 1
+//     setRight(updatedRight)
+//     setTotal(updatedRight + left)
+//   }
+
+
+//   return (
+//     <div>
+//       {left}
+//       <Button handleClick={handleLeftClick} text='left' />
+//       <Button handleClick={handleRightClick} text='right' />
+//       {right}
+//       <History allClicks={allClicks}/>
+//       <p>total {total}</p>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+
+
+import { useState } from 'react'
+
+const Display = props => <div>{props.value}</div>
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
+)
+
 const App = () => {
-  const friends = [
-    { name: 'Peter', age: 4 },
-    { name: 'Maya', age: 10 }
-  ]
+  const [value, setValue] = useState(10)
+
+  const setToValue = (newValue) => {
+    console.log('value now', newValue)
+    setValue(newValue)
+  }
 
   return (
     <div>
-      <p>{friends[0].name} {friends[0].age}</p>
-      <p>{friends[1].name} {friends[1].age}</p>
+      <Display value={value} />
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
+      <Button handleClick={() => setToValue(0)} text="reset" />
+      <Button handleClick={() => setToValue(value + 1)} text="increment" />
     </div>
   )
 }
 
 export default App
+
+
+
+
--- Other_files/part1/src/main.jsx
+++ Other_files/part1/src/main.jsx
@@ -3,3 +3,27 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+
+
+// ===================================
+// PART 1C
+// ===================================
+
+// import ReactDOM from 'react-dom/client'
+
+// import App from './App'
+
+// let counter = 1
+
+
+
+// const refresh = () => {
+//     ReactDOM.createRoot(document.getElementById('root')).render(
+//         <App counter={counter} />
+//     )
+// }
+
+// setInterval(() => {
+//     refresh()
+//     counter += 1
+// }, 1000)
