c9086fb5d0e998724e14c0ae8b18de8ba8decb47
Marwinez <gietus579@gmail.com>
2024-08-27 19:46:22 +0200

    Modified Course.jsx in part2/courseinfo

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,37 +1,19 @@
-const Header = ({ content }) => {
-    return (
-      <h2>{content}</h2>
-    )
-  }
+const Header = ({ content }) => <h2>{content}</h2>
   
-  const Part = ({ part }) => {
-    return (
-      <p>{part.name} {part.exercises}</p>
-    )
-  }
+const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
   
-  const Content = ({ parts }) => {
-    return (
-      <div>
-        {parts.map(part => <Part key={part.id} part={part} />)}
-      </div>
-    )
-  }
-  
-  const Total = ({ parts }) => {
-    return (
-      <p><strong>total of {parts.reduce((sum, part) => sum + part.exercises, 0)} exercises</strong></p>
-    )
-  }
-  
-  const Course = ({ course }) => {
-    return (
-      <>
-        <Header content={course.name} />
-        <Content parts={course.parts} />
-        <Total parts={course.parts} />
-      </>
-    )
-  }
+const Content = ({ parts }) => <div>{parts.map(part => <Part key={part.id} part={part} />)}</div>
 
-  export default Course
+const Total = ({ parts }) => <p><strong>total of {parts.reduce((sum, part) => sum + part.exercises, 0)} exercises</strong></p>
+  
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header content={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </>
+  )
+}
+
+export default Course
