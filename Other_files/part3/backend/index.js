3e916c76698aef5629b9cfd74a0844ed1eec45e8
Marwinez <gietus579@gmail.com>
2024-09-12 15:48:16 +0200

    smol update to backend port log

--- Other_files/part3/backend/index.js
+++ Other_files/part3/backend/index.js
@@ -94,7 +94,7 @@ app.post('/api/notes', (request, response) => {
 app.use(unknownEndpoint)
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
--- Other_files/part3/frontend/eslint.config.js
+++ Other_files/part3/frontend/eslint.config.js
@@ -34,5 +34,6 @@ export default [
         { allowConstantExport: true },
       ],
     },
+    'prop/types': 0
   },
 ]
