dc6e8dd369b729d9831b36020aa66ecee5c23729
Marwinez <gietus579@gmail.com>
2024-09-12 15:44:30 +0200

    Added frontend to notesapp in Other_files/part3

--- Other_files/part3/backend/index.js
+++ Other_files/part3/backend/index.js
@@ -1,7 +1,23 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+
+app.use(cors())
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
 
 app.use(express.json())
+app.use(requestLogger)
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
 let notes = [
     {
@@ -74,6 +90,9 @@ app.post('/api/notes', (request, response) => {
 
   response.json(note)
 })
+
+app.use(unknownEndpoint)
+
 
 const PORT = 3001
 app.listen(PORT, () => {
